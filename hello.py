4677e5ce4f5492b7cb559caa4e6646fa55edbf1a
Fuhito Suguri <bitwalk@gmail.com>
2021-03-28 19:30:14 +0900

    update

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import sys
+import PySide6
 from PySide6 import QtCore
 from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
@@ -14,14 +15,17 @@ class Hello(QWidget):
         self.setWindowTitle('Hello World!')
         self.show()
 
+        print('PySide', PySide6.__version__)
+        print('Qt', PySide6.QtCore.__version__)
+
     def initUI(self):
-        label: QLabel = QLabel('こんにちは、世界！')
+        label = QLabel('こんにちは、世界！')
         font: QFont = QFont()
         font.setPointSize(24)
         label.setFont(font)
         label.setAlignment(QtCore.Qt.AlignCenter)
 
-        layout: QVBoxLayout = QVBoxLayout()
+        layout = QVBoxLayout()
         layout.addWidget(label)
         self.setLayout(layout)
 
@@ -30,7 +34,6 @@ def main():
     app: QApplication = QApplication(sys.argv)
     ex: Hello = Hello()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
