725943c4052e9488e2bb0f8c6c4873276bb8722f
Fuhito Suguri <bitwalk@gmail.com>
2021-03-29 06:56:10 +0900

    update

---
+++ qt_label.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QVBoxLayout,
+    QWidget,
+)
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setWindowTitle('Label')
+        self.show()
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        lab = QLabel('ラベルは文字を表示します。')
+        vbox.addWidget(lab)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
