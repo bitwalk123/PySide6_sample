0b6ee77381b6a22f5f8be39907a45c0e75843592
Fuhito Suguri <bitwalk@gmail.com>
2021-03-29 06:52:10 +0900

    update

---
+++ qt_pushbutton.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QSizePolicy,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setWindowTitle('PushButton')
+        self.show()
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        btn = QPushButton('プッシュボタン')
+        btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        btn.clicked.connect(self.buttonClicked)
+        vbox.addWidget(btn)
+
+    def buttonClicked(self):
+        obj = self.sender()
+        print('「' + obj.text() + '」がクリックされました。')
+
+
+def main():
+    app: QApplication = QApplication(sys.argv)
+    ex: Example = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
