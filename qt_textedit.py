58d2d88065eb07c6c93e3f240497b6608c9bd36d
Fuhito Suguri <bitwalk@gmail.com>
2021-05-13 11:46:25 +0900

    update

--- qt_textedit.py
+++ qt_textedit.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Reference:
+# https://pythonpyqt.com/pyqt-qtextedit/
 
 import sys
 from PySide6.QtWidgets import (
     QApplication,
+    QMainWindow,
     QPushButton,
     QTextEdit,
     QVBoxLayout,
@@ -11,33 +14,39 @@ from PySide6.QtWidgets import (
 )
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.textEdit = QTextEdit()
-        self.btnPress1 = QPushButton("Button 1")
-        self.btnPress2 = QPushButton("Button 2")
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.textEdit)
-        layout.addWidget(self.btnPress1)
-        layout.addWidget(self.btnPress2)
-        self.setLayout(layout)
-
-        self.btnPress1.clicked.connect(self.btnPress1_Clicked)
-        self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+        self.initUI()
 
         self.setWindowTitle("QTextEdit")
         self.resize(300, 270)
         self.show()
 
+    def initUI(self):
+        base = QWidget()
+        self.setCentralWidget(base)
 
-    def btnPress1_Clicked(self):
-        self.textEdit.setPlainText("Hello PySide6!\nfrom pythonpyqt.com")
+        tedit = QTextEdit()
+        btn1 = QPushButton("Button 1")
+        btn2 = QPushButton("Button 2")
 
-    def btnPress2_Clicked(self):
-        self.textEdit.setHtml("<font color='red' size='6'><red>Hello PySide6!\nHello</font>")
+        layout = QVBoxLayout()
+        base.setLayout(layout)
+
+        layout.addWidget(tedit)
+        layout.addWidget(btn1)
+        layout.addWidget(btn2)
+
+        btn1.clicked.connect(lambda: self.btn1_Clicked(tedit))
+        btn2.clicked.connect(lambda: self.btn2_Clicked(tedit))
+
+    def btn1_Clicked(self, textEdit):
+        textEdit.setPlainText("Hello PySide6!\nfrom pythonpyqt.com")
+
+    def btn2_Clicked(self, textEdit):
+        textEdit.setHtml("<font color='red' size='6'><red>Hello PySide6!\nHello</font>")
 
 
 def main():
