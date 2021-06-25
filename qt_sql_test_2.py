6aad0867c46be21e57355303156e0cf072b784b1
Fuhito Suguri <bitwalk123@users.noreply.github.com>
2021-06-26 06:54:11 +0900

    update

---
+++ qt_sql_test_2.py
@@ -0,0 +1,54 @@
+# Reference
+# https://realpython.com/python-pyqt-database/
+import sys
+
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QMessageBox,
+    QTableWidget,
+    QTableWidgetItem,
+)
+
+
+class Contacts(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('QTableView Example')
+        self.resize(450, 250)
+        # Set up the view and load the data
+        self.view = QTableWidget()
+        self.view.setColumnCount(4)
+        self.view.setHorizontalHeaderLabels(['ID', 'Name', 'Job', 'Email'])
+        query = QSqlQuery('SELECT id, name, job, email FROM contacts')
+        while query.next():
+            rows = self.view.rowCount()
+            self.view.setRowCount(rows + 1)
+            self.view.setItem(rows, 0, QTableWidgetItem(str(query.value(0))))
+            self.view.setItem(rows, 1, QTableWidgetItem(query.value(1)))
+            self.view.setItem(rows, 2, QTableWidgetItem(query.value(2)))
+            self.view.setItem(rows, 3, QTableWidgetItem(query.value(3)))
+        self.view.resizeColumnsToContents()
+        self.setCentralWidget(self.view)
+
+
+def createConnection():
+    con = QSqlDatabase.addDatabase('QSQLITE')
+    con.setDatabaseName('contacts.sqlite')
+    if not con.open():
+        QMessageBox.critical(
+            None,
+            'QTableView Example - Error!',
+            'Database Error: %s' % con.lastError().databaseText(),
+        )
+        return False
+    return True
+
+
+app = QApplication(sys.argv)
+if not createConnection():
+    sys.exit(1)
+win = Contacts()
+win.show()
+sys.exit(app.exec_())
