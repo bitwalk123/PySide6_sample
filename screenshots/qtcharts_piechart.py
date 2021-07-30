d7f54a049fd476fd55a6ac18067ad262b13606b1
Fuhito Suguri <bitwalk@gmail.com>
2021-07-30 12:13:06 +0900

    update

---
+++ screenshots/qtcharts_piechart.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Reference
+#   https://doc.qt.io/qt-6/qtcharts-piechart-example.html
+import sys
+from PySide6.QtCharts import (
+    QBarCategoryAxis,
+    QBarSet,
+    QBarSeries,
+    QChart,
+    QChartView,
+    QPieSeries,
+    QPieSlice,
+    QValueAxis,
+)
+from PySide6.QtCore import Qt
+from PySide6.QtGui import (
+    QPainter,
+    QPen
+)
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+)
+
+
+class PieChart(QChartView):
+    def __init__(self):
+        super().__init__()
+        chart = self.init_ui()
+        self.setChart(chart)
+        self.setRenderHint(QPainter.Antialiasing)
+
+    def init_ui(self):
+        series = QPieSeries()
+        series.append('Jane', 1)
+        series.append('Joe', 2)
+        series.append('Andy', 3)
+        series.append('Barbara', 4)
+        series.append('Axel', 5)
+
+        slice1 = series.slices()[1]
+        slice1.setExploded()
+        slice1.setLabelVisible()
+        slice1.setPen(QPen(Qt.darkGreen, 2))
+        slice1.setBrush(Qt.green)
+
+        chart = QChart()
+        chart.addSeries(series)
+        chart.setTitle('Simple piechart example')
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+
+        chart.legend().setVisible(True)
+        chart.legend().setAlignment(Qt.AlignBottom)
+
+        return chart
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        piechart = PieChart()
+        self.setCentralWidget(piechart)
+        self.resize(400, 400)
+        self.setWindowTitle('PieChart')
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
