6f1c96a8a1650267f70120af18edabedbc8063db
bitwalk <bitwalk@gmail.com>
2021-12-02 20:56:54 +0900

    update

--- qt_matplotlib_scatter.py
+++ qt_matplotlib_scatter.py
@@ -17,38 +17,8 @@ from matplotlib.widgets import EllipseSelector, RectangleSelector
 import seaborn as sns
 
 
-def select_callback(eclick, erelease):
-    x1, y1 = eclick.xdata, eclick.ydata
-    x2, y2 = erelease.xdata, erelease.ydata
-    print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
-
-
 def toggle_selector(event):
-    toggle_selector.RS.set_active(True)
-
-
-def chart():
-    fig, ax = plt.subplots()
-
-    x = np.random.rand(100)
-    y = np.random.rand(100)
-    ax = sns.scatterplot(x=x, y=y)
-    canvas = FigureCanvas(fig)
-
-    toggle_selector.RS = RectangleSelector(
-        ax, select_callback, useblit=True,
-        button=[1],  # disable middle & right buttons
-        minspanx=5, minspany=5, spancoords='pixels', interactive=True,
-        props=dict(
-            facecolor='pink',
-            edgecolor='red',
-            alpha=0.2,
-            fill=True
-        )
-    )
-    fig.canvas.mpl_connect('key_press_event', toggle_selector)
-
-    return canvas
+    toggle_selector.RS.set_acstive(True)
 
 
 class Example(QMainWindow):
@@ -58,7 +28,7 @@ class Example(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-        canvas = chart()
+        canvas = self.chart()
         self.setCentralWidget(canvas)
 
         navtoolbar = NavigationToolbar(canvas, self)
@@ -66,6 +36,34 @@ class Example(QMainWindow):
             Qt.BottomToolBarArea,
             navtoolbar
         )
+
+    def chart(self):
+        fig, ax = plt.subplots()
+
+        x = np.random.rand(100)
+        y = np.random.rand(100)
+        ax = sns.scatterplot(x=x, y=y)
+        canvas = FigureCanvas(fig)
+
+        toggle_selector.RS = RectangleSelector(
+            ax, self.select_callback, useblit=True,
+            button=[1],  # disable middle & right buttons
+            minspanx=5, minspany=5, spancoords='pixels', interactive=True,
+            props=dict(
+                facecolor='pink',
+                edgecolor='red',
+                alpha=0.2,
+                fill=True
+            )
+        )
+        fig.canvas.mpl_connect('key_press_event', toggle_selector)
+
+        return canvas
+
+    def select_callback(self, eclick, erelease):
+        x1, y1 = eclick.xdata, eclick.ydata
+        x2, y2 = erelease.xdata, erelease.ydata
+        print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
 
 
 def main():
