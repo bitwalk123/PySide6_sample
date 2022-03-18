85efeac5cb746f755549a64c887b0913f214b488
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-03-18 09:14:48 -0700

    update

--- calculator.py
+++ calculator.py
@@ -5,8 +5,8 @@ import math
 import queue
 import re
 import sys
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QIcon
+from PySide6.QtCore import Qt, QByteArray
+from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtWidgets import (
     QApplication,
     QGridLayout,
@@ -61,7 +61,7 @@ class Calculator(QWidget):
         super().__init__()
         self.init_ui()
         self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('calculator.png'))
+        self.setWindowIcon(QIcon(self.get_app_pixmap()))
         self.setWindowFlags(Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint)
 
     def init_ui(self):
@@ -94,6 +94,14 @@ class Calculator(QWidget):
             but.setStyleSheet("QPushButton {font-size:12pt; padding:5px 30px; color:darkgreen;}")
             but.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
             grid.addWidget(but, key['y'], key['x'], key['h'], key['w'])
+
+    def get_app_pixmap(self):
+        base64data = b'iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAYAAADDPmHLAAAACXBIWXMAAAsTAAALEwEAmpwYAAAHY0lEQVR4nO2dW2wUVRjH0QiIioDxycQEiYmm3Ha7tIspWInAznS3LaXddntvoTvTIndB9KkmBuILhFoUfTExXhMTgcQEgoI3UAFDUMTEUERAUaKGAH3ACB7P2bjD7ux1tjv7ne78f8mXZlN2+Hr+vzlz5ux2O2YMAAAAAAAAAAAAAAAAAAAAAAAAAAAAmfCo2ky3qg/w+p7XMC822sqlat+6FL0k4WcLaPe7Ve0T/r0bBehjODKGirZ9lr93BkWWlnhYWTWeN/wKb/gmdYB5kmCP+Wd0K/pmkl64cLx2lASD4yiyzUgkfEU/SB1aPmvtJh9jZ8bE1Y6BMlopFe2AlBJwO3dSB5avKq3SWVO4gf10eEqCAJe/u5NpawJsjj9MKcEgdd5xRK75pmnft7SD7e0vZ3+9/BC7/tqD0pV5UM1By1DDP4xlB3dNZXVdTSYB9BuzfT3TqXM3EIsUc/iXBqeRhzzaBYjWlZPjmdLSGi+Bqm2jzt2Ar4pPxTYnznzqgItJAFH7358W37OinaTO3YBPSVdjm5N12h/NAgyfGme+DFylzt3APJjU4RajAKLMPVPnbgABIAAEgAAQAAIUsQB/HL+Lrd6osooly9jKp6vY79/cM6Iwz345mXWsWMLKAuHI8f48MQECyCzAivX+uH/vb2thvx6dmFP4YodxYWNH3PHWPZO47QwBJBHg5tBtkTPV/JxcJDh7eDJb2NSecKzHl3ZDAFkFENXJp2vzc6xKIKb9RaHE8EWt2qBCAJkF+PmrSQnTthUJzn09iS1ubkv5/GzWFBCAUIDoGZwqRLWllf1y9N6kzzvPw/eFrD8PAkgmQHQmsBLmxWMTI2f4SMOHAJIIEJ3Os5HgNz6tB9rzEz4EkEiATBKIM/7YvgdYVWvy8HO9hYQAEgkgSoSYanpPVbmc+UUnwNDWR1l3W5CV+zVLg5epxPGWtQfZmW2PFESA6DU+1TSfz/CLSoCe9oa8Bm8urbOhYAIYErQ12xq+7AJciDYl3g6WafDn1yy3VYDKmmUFFUCUuOan60l8f6T/h7QCuNSwKiQQ4X/6QqnjZoDIaj+LGeDCkSKdAaJkuwArpjVANuFHS7yxU2wKOV4AGSofAlgJ35CguS1nCaQVYG5w3QSXovW982wFu7JzKnm4hRAgXfhiuhfX/FTfF68GnknySyejVgC3ou2ONrV2eS15uHYLkC58xbwTmEqCxg7LEkgsgP537HWYOlw7BcgUvnmhl0mCoUPZSyCvAKbGqMO1SwCr4WfzvCe5BKcP3QcBZBdAvF6fattXyWKTJ922sZgJxKuMEEBiAfrW+ZOHz1f12d7fp5Og+6laCCCzAN6anqThW72lSyWBt7qH/TsEAaQVwPyuYPFScK7388kkEG85xwwgsQCXjt8duQyIMzW8unrEL+yINYV4I+j8pd1s/SYffi9AdgFkKAgAASAABIAAEAACQAAIAAEgAASQBAgAARKaQ9lf1LkbUA+EU4s6dwPqgXBqUeduQD0QTi3q3A3MjbUeOSd9mXtW9qrSFwSAABAAAkAACAABnCnAnOCtt6W5VO08de4GEKAwteDVuogEInx3laZQ524AAQpb1HknAAEKVz4hQH//7dSZx2GXAMG9J9i8vi3MU7c68lU8drIATwzWM0+txtyKftmt9tZT525glwAVvZvjjiseO1kAT33sIlC/SJ27gV0ClAZWxB1XPHayAI67DUy2/w0BIAAEgACFF2DRuzXMu7yLlfqTvzKXa4njeXu62KL3qjP2UBqI/Uwl7Tp17gZOEMAb7spr8Oby6p0Ze3hsTcxH0yr629S5GzhBgNIl9v6xaA8/fsbLwIdVrHJ7g9gJbKms7L+DOncDJwggwwwQLeq8E3CCADKsAYpGALGjJzZ1zPf5Ix5MfryKvi2sMYsdQ6sCyFCLd/nZ/OdDzKVo/TP9fVOoczewKoDY1rVzOp238sWiFMCrdd7qWdH2U+duYFUAsbdvpwBz6tYUpQDumI/W5QvBf6hzN8AMUCABTD1T526ANQAEsCQA7gJipLVwFwABCAWQYR8AAhAKIMNOIATADAABqATAGiANThBAhoIAEMBZAuA9gQ4XoMK0YygeQwAHCSB29MS2rtjbF1+z2eGDAATYJYCdBQHyCASAABAAAkAAxwrgUvSrsY2FvjhNHnCxCeDbHf8na8SYU+du4Fa1U7HNVb+xnzzgYhNgwUv18T0r2knq3A14M9tjm5vb/hwLffYjecjFIoDvg0Dcx8NEZgBV20adu8Esf+8MPiXdiG2wLLSRVb++jzV/LuflYDQI4Nvjj5z5CeHzseZVQp17HLyhHeZBjVZ500ZW+9bBpEHUvHmAlTduSPo8VMoaoM47gZJgcJxL1T9O1XRZaENSAYQcEgzoaKqPPB5tLHXeSYlIoGiD5suBqFQfa8a/d0GCQZW+/h/TAWnDj2W2r2e6WKSIlSq/Q7iW7mPNXGpYhQSpSrsmxpCHv1W6az4AAAAAAAAAAAAAAAAAAAAAAAAAAJCT/wDRCjptVbAKIwAAAABJRU5ErkJggg=='
+        byte_array = QByteArray.fromBase64(base64data)
+        pixmap = QPixmap()
+        pixmap.loadFromData(byte_array)
+
+        return pixmap
 
     # -------------------------------------------------------------------------
     #  get_display_string
