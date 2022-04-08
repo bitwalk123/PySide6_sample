50ac0a8f361061ff5ed01faaab3826588539776f
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-04-08 11:08:25 +0900

    update

--- qt_matplotlib_scatter.py
+++ qt_matplotlib_scatter.py
@@ -6,7 +6,7 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication,
-    QMainWindow
+    QMainWindow, QSizePolicy
 )
 import sys
 import numpy as np
@@ -14,8 +14,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 from matplotlib.widgets import EllipseSelector, RectangleSelector
 import seaborn as sns
+
+
+def select_callback(eclick, erelease):
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+    print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
 
 
 class Example(QMainWindow):
@@ -28,38 +35,36 @@ class Example(QMainWindow):
         # sample data
         df = pd.DataFrame(np.random.random(size=(100, 2)), columns=['X', 'Y'])
 
-        canvas = self.chart(df)
+        # plot
+        canvas: FigureCanvas = Scatter(df)
         self.setCentralWidget(canvas)
 
         navtoolbar = NavigationToolbar(canvas, self)
-        self.addToolBar(
-            Qt.BottomToolBarArea,
-            navtoolbar
-        )
+        self.addToolBar(Qt.BottomToolBarArea, navtoolbar)
 
-    def chart(self, df):
-        fig, ax = plt.subplots()
 
-        ax = sns.scatterplot(data=df, x=df.columns[0], y=df.columns[1])
-        canvas = FigureCanvas(fig)
+class Scatter(FigureCanvas):
+    fig = Figure()
 
+    def __init__(self, df):
+        super().__init__(self.fig)
+        self.init_plot(df)
+
+    def init_plot(self, df):
+        # Seaborn Scatter
+        ax = self.fig.add_subplot(111)
+        ax = sns.scatterplot(data=df, x=df.columns[0], y=df.columns[1], ax=ax)
+        ax.set(title='Scatter Sample')
+        # GUI setting
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        # Selector
         plt.RS = RectangleSelector(
-            ax, self.select_callback, useblit=True,
+            ax, select_callback, useblit=True,
             button=[1],  # disable middle & right buttons
             minspanx=5, minspany=5, spancoords='pixels', interactive=True,
-            props=dict(
-                facecolor='pink',
-                edgecolor='red',
-                alpha=0.2,
-                fill=True
-            )
+            props=dict(facecolor='pink', edgecolor='red', alpha=0.2, fill=True)
         )
-        return canvas
-
-    def select_callback(self, eclick, erelease):
-        x1, y1 = eclick.xdata, eclick.ydata
-        x2, y2 = erelease.xdata, erelease.ydata
-        print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
 
 
 def main():
