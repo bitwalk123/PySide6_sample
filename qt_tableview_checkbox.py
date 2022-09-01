9216c9a0b637627864ccdfdeb40ffbe8a60c880f
Fuhito Suguri <bitwalk@gmail.com>
2022-09-01 20:15:03 +0900

    update

--- qt_tableview_checkbox.py
+++ qt_tableview_checkbox.py
@@ -1,84 +1,63 @@
-# Reference:
-# https://stackoverflow.com/questions/17748546/pyqt-column-of-checkboxes-in-a-qtableview
+#!/usr/bin/env python
+# coding: utf-8
+
 import sys
 
-from PySide6.QtCore import (
-    Qt,
-    QEvent,
-    QModelIndex,
+from PySide6.QtGui import (
+    QStandardItem,
+    QStandardItemModel, Qt,
 )
-from PySide6.QtGui import QStandardItemModel
 from PySide6.QtWidgets import (
     QApplication,
-    QItemDelegate,
     QMainWindow,
-    QTableView,
+    QTableView, QHeaderView,
 )
-
-
-class CheckBoxDelegate(QItemDelegate):
-    """
-    A delegate that places a fully functioning QCheckBox cell of the column to which it's applied.
-    """
-
-    def __init__(self, parent):
-        QItemDelegate.__init__(self, parent)
-
-    def createEditor(self, parent, option, index):
-        """
-        Important, otherwise an editor is created if the user clicks in this cell.
-        """
-        return None
-
-    def paint(self, painter, option, index):
-        """
-        Paint a checkbox without the label.
-        """
-        self.drawCheck(painter, option, option.rect, Qt.Unchecked if int(index.data()) == 0 else Qt.Checked)
-
-    def editorEvent(self, event, model, option, index):
-        """
-        Change the data in the model and the state of the checkbox
-        if the user presses the left mousebutton and this cell is editable. Otherwise do nothing.
-        """
-        if not int(index.flags() & Qt.ItemIsEditable) > 0:
-            return False
-
-        if event.type() == QEvent.MouseButtonRelease and event.button() == Qt.LeftButton:
-            # Change the checkbox-state
-            self.setModelData(None, model, index)
-            return True
-
-        return False
-
-    def setModelData(self, editor, model, index):
-        """
-        The user wanted to change the old state in the opposite.
-        """
-        model.setData(index, 1 if int(index.data()) == 0 else 0, Qt.EditRole)
 
 
 class Example(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    list_label = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+    def __init__(self):
+        super().__init__()
         self.init_ui()
-        self.setWindowTitle('TableView, CheckBoxDelegate')
+        self.setWindowTitle('TableView with CheckBox')
 
     def init_ui(self):
         """
         initialize UI
         """
         tblview = QTableView()
-        self.setCentralWidget(tblview)
-        model = QStandardItemModel(4, 3)
+        tblview.horizontalHeader().setSectionResizeMode(
+            QHeaderView.ResizeToContents
+        )
+        model = QStandardItemModel()
         tblview.setModel(model)
+        self.setCentralWidget(tblview)
 
-        delegate = CheckBoxDelegate(None)
-        tblview.setItemDelegateForColumn(1, delegate)
-        for row in range(4):
-            for column in range(3):
-                index = model.index(row, column, QModelIndex())
-                model.setData(index, 1)
+        for row in range(len(self.list_label)):
+            list_row = list()
+            for column in range(10):
+                item = QStandardItem()
+                if column == 0:
+                    item.setText(self.list_label[row])
+                else:
+                    item.setCheckable(True)
+                    item.setCheckState(Qt.CheckState.Checked)
+                    item.setEditable(False)
+                list_row.append(item)
+            model.appendRow(list_row)
+        model.itemChanged.connect(self.on_check_item)
+
+    def on_check_item(self, item: QStandardItem):
+        if item.isCheckable():
+            row = item.row()
+            col = item.column()
+            if item.checkState() == Qt.CheckState.Checked:
+                msg = 'checked'
+            else:
+                msg = 'unchecked'
+
+            print('(%d, %d) -> %s' % (row, col, msg))
 
 
 def main():
