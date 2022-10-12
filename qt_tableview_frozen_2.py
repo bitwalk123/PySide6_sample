08256898f63925149cad3ed84879540791e9d52a
Fuhito Suguri <keiichi.takahashi@tel.com>
2022-10-12 17:49:58 +0900

    update

--- qt_tableview_frozen_2.py
+++ qt_tableview_frozen_2.py
@@ -2,20 +2,72 @@
 # coding: utf-8
 
 import sys
+from typing import Any
 
 from PySide6.QtCore import (
     Qt,
-    QAbstractTableModel, QSortFilterProxyModel,
+    QAbstractTableModel, QSortFilterProxyModel, QModelIndex, QPersistentModelIndex,
 )
 from PySide6.QtWidgets import (
     QApplication,
     QHeaderView,
     QMainWindow,
-    QTableView, QAbstractItemView,
+    QTableView, QAbstractItemView, QProxyStyle, QStyledItemDelegate,
 )
 
+class MyContents:
+    """
+    This class defines only column header and contents of label columns.
+    It is assumed that label columns are left side of columns and rest of columns are checkbox.
+    """
+    # CheckBox status used by QPersistentModelIndex
+    check_states = dict()
 
-class MyTableModel(QAbstractTableModel):
+    def __init__(self, list_labels: list, header_labels: list):
+        self.list_labels = list_labels
+        self.header_labels = header_labels
+
+    def getCheckColStart(self):
+        return len(self.list_labels)
+
+    def getCols(self):
+        return len(self.header_labels)
+
+    def getColumnHeader(self, index: int):
+        return self.header_labels[index]
+
+    def getData(self, row: int, col: int):
+        if col < self.getCheckColStart():
+            return self.list_labels[col][row]
+        else:
+            return None
+
+    def getRowIndex(self, index: int):
+        return str(index + 1)
+
+    def getRows(self):
+        return len(self.list_labels[0])
+
+
+class ProxyStyle4CheckBoxCenter(QProxyStyle):
+    def subElementRect(self, element, opt, widget=None):
+        if element == self.SE_ItemViewItemCheckIndicator:
+            rect = super().subElementRect(element, opt, widget)
+            rect.moveCenter(opt.rect.center())
+            return rect
+        return super().subElementRect(element, opt, widget)
+
+
+class CheckBoxDelegate(QStyledItemDelegate):
+    def initStyleOption(self, option, index: QModelIndex):
+        value = index.data(Qt.CheckStateRole)
+        if value is None:
+            model = index.model()
+            model.setData(index, Qt.Unchecked, Qt.CheckStateRole)
+        super().initStyleOption(option, index)
+
+
+class MyTableModel0(QAbstractTableModel):
     def __init__(self, parent=None, *args):
         QAbstractTableModel.__init__(self, parent, *args)
         self.colLabels = ['Col%02d' % i for i in range(1, 21)]
@@ -68,6 +120,53 @@ class MyTableModel(QAbstractTableModel):
 
         return Qt.ItemIsEnabled | Qt.ItemIsSelectable
 
+class MyTableModel(QAbstractTableModel):
+    def __init__(self, data: MyContents):
+        super(MyTableModel, self).__init__()
+        self._data = data
+        # self.check_states = dict()
+
+    def rowCount(self, index: QModelIndex = None):
+        return self._data.getRows()
+
+    def columnCount(self, index: QModelIndex = None):
+        return self._data.getCols()
+
+    def data(self, index: QModelIndex, role: Qt.ItemDataRole):
+        if role == Qt.DisplayRole:
+            row = index.row()
+            column = index.column()
+            value = self._data.getData(row, column)
+            return value
+
+        if role == Qt.CheckStateRole:
+            value = self._data.check_states.get(QPersistentModelIndex(index))
+            if value is not None:
+                return value
+
+    def setData(self, index: QModelIndex, value: Any, role: Qt.ItemDataRole = Qt.EditRole):
+        if role == Qt.CheckStateRole:
+            self._data.check_states[QPersistentModelIndex(index)] = value
+            self.dataChanged.emit(index, index, (role,))
+            return True
+
+        return False
+
+    def flags(self, index: QModelIndex):
+        return (
+                Qt.ItemIsEnabled
+                | Qt.ItemIsSelectable
+                | Qt.ItemIsUserCheckable
+        )
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: Qt.ItemDataRole):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self._data.getColumnHeader(section)
+            elif orientation == Qt.Vertical:
+                return self._data.getRowIndex(section)
+
 
 class FrozenTableView(QTableView):
     def __init__(self, parent=None):
@@ -88,7 +187,7 @@ class FTableView(QTableView):
         QTableView.__init__(self, parent, *args)
 
         # set the table model
-        #tm = MyTableModel(self)
+        # tm = MyTableModel(self)
         self.setModel(model)
 
         self.setMinimumSize(800, 600)
@@ -152,14 +251,26 @@ class FTableView(QTableView):
 
 
 class Example(QMainWindow):
+    #  Sample Data preparation
+    num_data = 1000
+    names = ['TEST' + str(x + 1).zfill(5) for x in range(num_data)]
+    units = ['unit' + str(x + 1).zfill(5) for x in range(num_data)]
+    list_label_names = [names, units]
+    num_check = 20
+    col_labels = ['NAME', 'UNIT'] + ['check(%d)' % (x + 1) for x in range(num_check)]
+
     def __init__(self):
         super().__init__()
         self.init_ui()
         self.setWindowTitle('FrozenTableView')
 
     def init_ui(self):
-        model = MyTableModel(self)
+        #model = MyTableModel(self)
+        contents = MyContents(self.list_label_names, self.col_labels)
+        model = MyTableModel(contents)
         table = FTableView(model)
+        table.setStyle(ProxyStyle4CheckBoxCenter())
+        self.setCentralWidget(table)
         # table.resizeColumnsToContents()
 
         head_horizontal = table.horizontalHeader()
@@ -167,8 +278,11 @@ class Example(QMainWindow):
 
         # set table model
         # table.setModel(SimpleTableModel(self.prefdata, self.header))
+        # delegate custom
+        delegate = CheckBoxDelegate(table)
+        for col in range(len(self.list_label_names), len(self.col_labels)):
+            table.setItemDelegateForColumn(col, delegate)
 
-        self.setCentralWidget(table)
 
 
 def main():
