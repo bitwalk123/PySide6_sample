940d43eb430db2f01a8f739ed79aec423689f8ed
Fuhito Suguri <keiichi.takahashi@tel.com>
2023-02-07 09:28:49 +0900

    update

--- qt_plugin_test.py
+++ qt_plugin_test.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 import importlib.util
 import sys
+from typing import Any
 
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import (
@@ -14,13 +15,39 @@ from PySide6.QtWidgets import (
 )
 
 
+def add_module(module_name: str, file_path: str, class_name) -> Any:
+    """Add module
+
+    Args:
+        module_name (str): module name to add
+        file_path (str): script path to load as plugin
+        class_name (str): class name in the module to use
+
+    Returns:
+        Any: plugin instance to add into main application
+
+    Note:
+        https://docs.python.org/3/library/importlib.html
+    """
+
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    # example: obj = module.Plugin()
+    obj = eval('module.%s()' % class_name)
+    print(type(obj))
+    return obj
+
+
 class Example(QMainWindow):
     layout: QVBoxLayout = None
     class_name = 'Plugin'
 
     def __init__(self):
         super().__init__()
-        self.counter_plugin = 0
+        self.id_plugin = 0
         self.init_ui()
         self.setWindowTitle('Plugin Test')
 
@@ -43,37 +70,24 @@ class Example(QMainWindow):
         file_menu.addAction(open_file)
 
     def plugin_read_dialog(self):
+        """Dialog to read plugin script
+        """
         dialog = QFileDialog()
         if dialog.exec():
             filename = dialog.selectedFiles()[0]
-            print(filename)
-            obj = self.add_module(filename)
-            self.layout.addWidget(obj)
+            self.add_plugin_widget(filename)
 
-    def add_module(self, file_path: str) -> QPushButton:
-        """Add module as plugin.
+    def add_plugin_widget(self, filename: str):
+        """Add plugin widget from specified filename
 
         Args:
-            file_path: script path to load as plugin
-
-        Returns:
-            QPushButton: plugin widget to add main application
-
-        Note:
-            https://docs.python.org/3/library/importlib.html
+            filename (str): plugin script to add
         """
-        module_name = 'plugin_%d' % self.counter_plugin
-        self.counter_plugin += 1
+        plugin_name = 'plugin_%d' % self.id_plugin
+        self.id_plugin += 1
 
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
-
-        # example: obj = module.Plugin()
-        obj = eval('module.%s()' % self.class_name)
-        print(type(obj))
-        return obj
+        obj = add_module(plugin_name, filename, self.class_name)
+        self.layout.addWidget(obj)
 
 
 def main():
