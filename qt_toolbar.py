333cdd3ce780834dd313edc273d11938bc08ff34
Keiichi Takahashi <bitwalk@gmail.com>
2023-05-05 09:31:55 +0900

    update

--- qt_toolbar.py
+++ qt_toolbar.py
@@ -24,19 +24,25 @@ class Example(QMainWindow):
 
         # Add buttons to toolbar
         but_open = QToolButton()
+        but_open.setText('Open')
+        but_open.setToolTip('Open file to contents.')
         pixmap_open = QStyle.StandardPixmap.SP_DirOpenIcon
         icon_open = self.style().standardIcon(pixmap_open)
         but_open.setIcon(icon_open)
+        but_open.clicked.connect(self.button_clicked)
         toolbar.addWidget(but_open)
 
-        but_play = QToolButton()
-        pixmap_play = QStyle.StandardPixmap.SP_MediaPlay
-        icon_play = self.style().standardIcon(pixmap_play)
-        but_play.setIcon(icon_play)
-        toolbar.addWidget(but_play)
+        but_save = QToolButton()
+        but_save.setText('Save')
+        but_save.setToolTip('Save contents as file.')
+        pixmap_save = QStyle.StandardPixmap.SP_DialogSaveButton
+        icon_save = self.style().standardIcon(pixmap_save)
+        but_save.setIcon(icon_save)
+        but_save.clicked.connect(self.button_clicked)
+        toolbar.addWidget(but_save)
 
         # spacer
-        spacer: QWidget = QWidget()
+        spacer = QWidget()
         spacer.setSizePolicy(
             QSizePolicy.Policy.Expanding,
             QSizePolicy.Policy.Expanding
@@ -44,8 +50,17 @@ class Example(QMainWindow):
         toolbar.addWidget(spacer)
 
         but_exit = QToolButton()
-        but_exit.setIcon(QIcon.fromTheme('application-exit'))
+        but_exit.setText('Exit')
+        but_exit.setToolTip('Exit application.')
+        pixmap_exit = QStyle.StandardPixmap.SP_BrowserStop
+        icon_exit = self.style().standardIcon(pixmap_exit)
+        but_exit.setIcon(icon_exit)
+        but_exit.clicked.connect(self.button_clicked)
         toolbar.addWidget(but_exit)
+
+    def button_clicked(self):
+        obj: QToolButton = self.sender()
+        print('%s button is clicked!' % obj.text())
 
 
 def main():
