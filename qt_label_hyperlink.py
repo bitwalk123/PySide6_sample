1febc5f569d1f0f560357140ebd43dcdb6ffcfb0
Keiichi Takahashi <bitwalk@gmail.com>
2023-08-14 13:13:22 +0900

    update

--- qt_label_hyperlink.py
+++ qt_label_hyperlink.py
@@ -10,17 +10,22 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
+
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.setWindowTitle('Label')
+        self.setWindowTitle('Label (Hyper Link)')
 
     def initUI(self):
         vbox = QVBoxLayout()
         self.setLayout(vbox)
 
-        lab = QLabel('ラベルは文字を表示します。')
+        url = 'https://doc.qt.io/qtforpython-6/'
+        lab = QLabel()
+        lab.setText('<a href="%s">%s</a>' % (url, url))
+        lab.setOpenExternalLinks(True)
+        lab.setContentsMargins(10, 0, 10, 0)
         lab.setSizePolicy(
             QSizePolicy.Policy.Expanding,
             QSizePolicy.Policy.Expanding
