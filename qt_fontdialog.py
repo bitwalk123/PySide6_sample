b40cdd977cbf2d86356e23f5178c6fdcccc10907
Fuhito Suguri <bitwalk@gmail.com>
2023-11-30 12:50:40 +0900

    update

---
+++ qt_fontdialog.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QFontDialog,
+    QPushButton,
+    QSizePolicy,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.setWindowTitle('font selection')
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        btn = QPushButton('Select Font')
+        btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        btn.clicked.connect(self.button_clicked)
+        layout.addWidget(btn)
+
+    def button_clicked(self):
+        button: QPushButton = self.sender()
+        ok, font = QFontDialog.getFont()
+        if ok:
+            button.setFont(font)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
