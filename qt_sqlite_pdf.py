4ff5ff30a0c0aff40596812e0db3ac29a7e8cd0e
Fuhito Suguri <bitwalk@gmail.com>
2023-12-19 19:08:45 +0900

    update

--- qt_sqlite_pdf.py
+++ qt_sqlite_pdf.py
@@ -19,6 +19,51 @@ from PySide6.QtWidgets import (
 )
 
 
+def create_table():
+    query = QSqlQuery()
+    sql = 'CREATE TABLE file (name_file TEXT UNIQUE, content NONE);'
+    if not query.exec(sql):
+        print(query.lastError())
+
+
+def get_content_from_filename(filename: str) -> bytes:
+    content = None
+    query = QSqlQuery()
+    sql = 'SELECT content FROM file WHERE name_file = "%s";' % filename
+    query.exec(sql)
+    if query.next():
+        content_str = query.value(0)
+        content = base64.b64decode(content_str.encode())
+    return content
+
+
+def get_list_file(list_file: list):
+    query = QSqlQuery()
+    sql = 'SELECT name_file FROM file;'
+    flag = query.exec(sql)
+    while query.next():
+        list_file.append(query.value(0))
+    if not flag:
+        print(query.lastError())
+
+
+def insert_filename_content(filename: str, content_str: str):
+    sql = 'INSERT INTO file VALUES(?, ?);'
+    query = QSqlQuery()
+    query.prepare(sql)
+    query.bindValue(0, filename)
+    query.bindValue(1, content_str)
+    """
+    note: This does not work!
+    query.bindValue(
+        1, content,
+        type=QSql.ParamTypeFlag.In | QSql.ParamTypeFlag.Binary
+    )
+    """
+    if not query.exec():
+        print(query.lastError())
+
+
 class Example(QMainWindow):
     dbname = 'test.sqlite'
     con = QSqlDatabase.addDatabase('QSQLITE')
@@ -32,8 +77,13 @@ class Example(QMainWindow):
 
         self.init_ui()
         self.update_filelist()
-        self.setWindowTitle('SQLite PDF Test')
+        self.setWindowTitle('SQLite & PDF Test')
         self.resize(600, 800)
+
+    def init_db(self):
+        if self.con.open():
+            create_table()
+            self.con.close()
 
     def init_ui(self):
         menubar = self.menuBar()
@@ -41,8 +91,8 @@ class Example(QMainWindow):
 
         file_open = QAction('Open', self)
         file_open.setShortcut('Ctrl+O')
-        file_open.setStatusTip('Open New File')
-        file_open.triggered.connect(self.show_dlg)
+        file_open.setStatusTip('open PDF file')
+        file_open.triggered.connect(self.show_dialog)
         menu_file.addAction(file_open)
 
         toolbar = QToolBar()
@@ -53,7 +103,7 @@ class Example(QMainWindow):
             QSizePolicy.Policy.Expanding,
             QSizePolicy.Policy.Preferred
         )
-        self.combo.currentTextChanged.connect(self.on_click_open)
+        self.combo.currentTextChanged.connect(self.on_current_text_changed)
         toolbar.addWidget(self.combo)
 
         view = QPdfView(self)
@@ -61,59 +111,41 @@ class Example(QMainWindow):
         view.setZoomMode(QPdfView.ZoomMode.FitToWidth)
         self.setCentralWidget(view)
 
-    def show_dlg(self):
+    def show_dialog(self):
         dialog = QFileDialog()
+        dialog.setWindowTitle('PDF file selection')
         dialog.setNameFilters(['PDF files (*.pdf)'])
         if dialog.exec():
             filename = dialog.selectedFiles()[0]
-            name_file = os.path.basename(filename)
+            basename = os.path.basename(filename)
             f = open(filename, 'rb')
             with f:
                 content = base64.b64encode(f.read())
-                print(type(content))
                 content_str: str = content.decode()
 
             if self.con.open():
-                sql = 'INSERT INTO file VALUES(?, ?);'
-                query = QSqlQuery()
-                query.prepare(sql)
-                query.bindValue(0, name_file)
-                # query.bindValue(1, content, type=QSql.ParamTypeFlag.In | QSql.ParamTypeFlag.Binary)
-                query.bindValue(1, content_str)
-                if not query.exec():
-                    print(query.lastError())
+                insert_filename_content(basename, content_str)
                 self.con.close()
                 self.update_filelist()
-                self.combo.setCurrentText(name_file)
+                self.combo.setCurrentText(basename)
 
     def update_filelist(self):
         list_name_file = list()
         if self.con.open():
-            query = QSqlQuery()
-            sql = 'SELECT name_file FROM file;'
-            query.exec(sql)
-            while query.next():
-                list_name_file.append(query.value(0))
+            get_list_file(list_name_file)
             self.con.close()
 
         self.combo.clear()
-        # self.combo.clearEditText()
         for name_file in list_name_file:
             self.combo.addItem(name_file)
 
-    def on_click_open(self):
+    def on_current_text_changed(self):
         filename = self.combo.currentText()
         if len(filename) == 0:
             return
 
         if self.con.open():
-            content = None
-            query = QSqlQuery()
-            sql = 'SELECT content FROM file WHERE name_file = "%s";' % filename
-            query.exec(sql)
-            if query.next():
-                content_str = query.value(0)
-                content = base64.b64decode(content_str.encode())
+            content = get_content_from_filename(filename)
             self.con.close()
 
             if content is not None:
@@ -124,13 +156,6 @@ class Example(QMainWindow):
                 document.load(filepath)
                 view: QWidget | QPdfView = self.centralWidget()
                 view.setDocument(document)
-
-    def init_db(self):
-        if self.con.open():
-            query = QSqlQuery()
-            sql = 'CREATE TABLE file (name_file TEXT UNIQUE, content NONE);'
-            query.exec(sql)
-            self.con.close()
 
 
 def main():
