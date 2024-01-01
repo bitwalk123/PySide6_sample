9f976d58ddf63bda2fc891b89d8580313368c8b2
Fuhito Suguri <bitwalk@gmail.com>
2024-01-02 08:59:14 +0900

    update

--- qt_toolbar_toolbutton_menu.py
+++ qt_toolbar_toolbutton_menu.py
@@ -13,6 +13,7 @@ from PySide6.QtWidgets import (
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.group_action: QActionGroup = None
         self.init_ui()
 
     def init_ui(self):
@@ -28,30 +29,29 @@ class Example(QMainWindow):
         toolbutton.setPopupMode(
             QToolButton.ToolButtonPopupMode.InstantPopup
         )
-        group_action = QActionGroup(self)
 
         action_1 = QAction('Action 1')
         action_1.setCheckable(True)
-        action_1.toggled.connect(self.on_toggled)
+        action_1.setChecked(True) # default selection
         toolmenu.addAction(action_1)
-        group_action.addAction(action_1)
 
         action_2 = QAction('Action 2')
         action_2.setCheckable(True)
-        action_2.toggled.connect(self.on_toggled)
         toolmenu.addAction(action_2)
-        group_action.addAction(action_2)
 
         action_3 = QAction('Action 3')
         action_3.setCheckable(True)
-        action_3.toggled.connect(self.on_toggled)
         toolmenu.addAction(action_3)
+
+        self.group_action = group_action = QActionGroup(self)
+        group_action.triggered.connect(self.on_triggered)
+        group_action.addAction(action_1)
+        group_action.addAction(action_2)
         group_action.addAction(action_3)
 
-    def on_toggled(self):
-        action: QAction = self.sender()
-        if action.isChecked():
-            print(action.text(), 'is selected.')
+    def on_triggered(self):
+        action = self.group_action.checkedAction()
+        print(action.text(), 'is selected.')
 
 
 def main():
