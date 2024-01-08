80c2d33e96e5b0c958a148de2ea70111aee64d86
Fuhito Suguri <bitwalk@gmail.com>
2024-01-08 13:04:45 +0900

    update

--- qt_webenginepage.py
+++ qt_webenginepage.py
@@ -3,40 +3,29 @@ import sys
 from PySide6.QtCore import QUrl
 from PySide6.QtWebEngineCore import QWebEnginePage
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QWidget,
-)
+from PySide6.QtWidgets import QApplication
 
 
-class Example(QMainWindow):
-
+class Example(QWebEngineView):
     def __init__(self, url: QUrl):
         super().__init__()
-        self.init_ui(url)
+        self.load(url)
+        self.loadFinished.connect(self.on_load_finished)
         self.resize(1000, 800)
-
-    def init_ui(self, url):
-        view = QWebEngineView()
-        view.load(url)
-        view.loadFinished.connect(self.on_load_finished)
-        self.setCentralWidget(view)
 
     def on_load_finished(self, flag: bool) -> bool:
         if not flag:
             return False
-        view: QWidget | QWebEngineView = self.centralWidget()
-        page: QWebEnginePage = view.page()
+        page: QWebEnginePage = self.page()
         page.runJavaScript(
-            'document.documentElement.outerHTML',
-            0, self.print_content
+            "document.documentElement.outerHTML",
+            0, self.print_html
         )
         return True
 
     @staticmethod
-    def print_content(content: str):
-        print(content)
+    def print_html(html: str):
+        print(html)
 
 
 def main(url: QUrl):
