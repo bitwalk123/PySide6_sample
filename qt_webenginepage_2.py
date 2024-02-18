0f718a44bf84bd6b7a7cb25e3bde2399652d6512
Fuhito Suguri <bitwalk@gmail.com>
2024-02-18 17:06:51 +0900

    update

--- qt_webenginepage_2.py
+++ qt_webenginepage_2.py
@@ -2,23 +2,41 @@
 # coding: utf-8
 import sys
 
-from PySide6.QtCore import QUrl
+from PySide6.QtCore import QUrl, QFileInfo
+from PySide6.QtGui import QAction
+from PySide6.QtWebEngineCore import QWebEnginePage, QWebEngineDownloadRequest
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QFileDialog
 
 
 class Example(QWebEngineView):
     def __init__(self, url: QUrl):
         super().__init__()
-        self.loadFinished.connect(self.on_load_finished)
         self.resize(1000, 800)
 
         self.load(url)
         self.page().titleChanged.connect(self.setWindowTitle)
+        self.page().profile().downloadRequested.connect(
+            self.on_downloadRequested
+        )
 
-    def on_load_finished(self, flag: bool):
-        if flag:
+    def createWindow(self, wwtype: QWebEnginePage.WebWindowType):
+        action: QAction = self.pageAction(QWebEnginePage.WebAction.ViewSource)
+        if action.isEnabled():
             self.page().toHtml(self.print_html)
+
+    def on_downloadRequested(self, download: QWebEngineDownloadRequest):
+        print(type(download))
+        # Reference:
+        # https://stackoverflow.com/questions/55963931/how-to-download-csv-file-with-qwebengineview-and-qurl
+        old_path = download.url().path()  # download.path()
+        suffix = QFileInfo(old_path).suffix()
+        path, _ = QFileDialog.getSaveFileName(
+            self, "Save File", old_path, "*." + suffix
+        )
+        if path:
+            download.setDownloadDirectory(path)
+            download.accept()
 
     @staticmethod
     def print_html(html: str):
