f95d5cbe78607c82e216039d8ac12d9774cf4dfa
Fuhito Suguri <bitwalk@gmail.com>
2024-03-16 14:57:21 +0900

    update

--- qt_tabwidget.py
+++ qt_tabwidget.py
@@ -2,46 +2,24 @@ import sys
 
 from PySide6.QtWidgets import (
     QApplication,
-    QHBoxLayout,
-    QLabel,
-    QPushButton,
     QTabWidget,
     QWidget,
 )
 
 
-class Tab1Widget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__()
-        closeBtn = QPushButton('Close')
-        closeBtn.clicked.connect(parent.close)
-        hbox = QHBoxLayout()
-        hbox.addWidget(closeBtn)
-        self.setLayout(hbox)
-
-
-class Example(QWidget):
+class Example(QTabWidget):
     def __init__(self):
         super().__init__()
-        self.init_ui()
         self.setWindowTitle('QTabWidget')
-        self.resize(400, 400)
-        self.show()
 
-    def init_ui(self):
-        tab = QTabWidget()
-        tab.addTab(Tab1Widget(parent=self), 'Tab1')
-        tab.addTab(QLabel('Label 2'), 'Tab2')
-
-        hbox = QHBoxLayout()
-        hbox.addWidget(tab)
-
-        self.setLayout(hbox)
+        self.addTab(QWidget(), 'タブ１')
+        self.addTab(QWidget(), 'タブ２')
 
 
 def main():
     app = QApplication(sys.argv)
-    ui = Example()
+    win = Example()
+    win.show()
     sys.exit(app.exec())
 
 
