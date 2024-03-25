82b13505c4a92a9494bb4231f262087185b619c0
Fuhito Suguri <bitwalk@gmail.com>
2024-03-26 07:09:30 +0900

    update

--- mpl_trend_03.py
+++ mpl_trend_03.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     df = pd.read_csv(csvfile, index_col=0, parse_dates=True)
 
     fig, ax = plt.subplots()
-    fig.canvas.manager.set_window_title('Trend test with spline curve')
+    fig.canvas.manager.set_window_title('Trend test with spline curve & derivative')
 
-    line1 = plt.plot(
+    line1 = ax.plot(
         df,
         linewidth=1,
         color='blue',
@@ -23,24 +23,38 @@ if __name__ == '__main__':
     )
 
     ts: Index = df.index.map(pd.Timestamp.timestamp)
-    print(ts)
     bspl: BSpline = make_interp_spline(ts, df['気温'], k=2)
+    dbspl = bspl.derivative(nu=1)
 
     x1: DatetimeIndex = pd.date_range(min(df.index), max(df.index), freq='1min')
     ts1: Index = x1.map(pd.Timestamp.timestamp)
     y1 = bspl(ts1)
+    dy1 = dbspl(ts1)
 
-    line2 = plt.plot(
+    line2 = ax.plot(
         x1, y1,
         linewidth=1,
         color='red',
         label='spline curve'
     )
 
-    plt.grid()
-    plt.xticks(rotation=45)
-    plt.ylabel('Temperature')
+    ax2 = ax.twinx()
+    line3 = ax2.plot(
+        x1, dy1,
+        linewidth=1,
+        linestyle='dashed',
+        color='violet',
+        label='derivative'
+    )
+
     fig.legend(loc='outside lower center')
 
-    plt.subplots_adjust(top=0.99, left=0.1, bottom=0.25, right=0.95)
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+    ax.set_ylabel('Temperature')
+    ax.grid()
+
+    ax2.set_ylabel('Derivative')
+
+    plt.subplots_adjust(top=0.99, left=0.1, bottom=0.3, right=0.8)
     plt.show()
