8832a4c67748307cdabc6372d6c4378c8120d4c9
Fuhito Suguri <bitwalk@gmail.com>
2024-06-14 18:55:22 +0900

    update

--- qt_matplotlib_zoom.py
+++ qt_matplotlib_zoom.py
@@ -1,13 +1,14 @@
 import sys
 from math import sin
+from typing import Any
 
-import matplotlib.pyplot as plt
 from PySide6.QtCore import Qt
-
+from matplotlib.axes import Axes
 from matplotlib.backends.backend_qtagg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar,
 )
+from matplotlib.collections import PathCollection
 from matplotlib.figure import Figure
 
 from PySide6.QtWidgets import QMainWindow, QApplication
@@ -16,55 +17,49 @@ from PySide6.QtWidgets import QMainWindow, QApplication
 class MyChart(FigureCanvas):
     def __init__(self):
         self.fig = Figure()
-        self.fig_width = self.fig.get_figwidth()
-        self.fig_height = self.fig.get_figheight()
-        self.fig.canvas.mpl_connect('resize_event', self.on_resize)
+        self.fig.canvas.mpl_connect('resize_event', self.onResize)
+        super().__init__(self.fig)
+
+        self.fig_w = self.fig.get_figwidth()
+        self.fig_h = self.fig.get_figheight()
+
         self.ax = self.fig.add_subplot(111)
+        self.ax.callbacks.connect('xlim_changed', self.limChanged)
+        self.ax.callbacks.connect('ylim_changed', self.limChanged)
+
         self.xlim = self.ax.get_xlim()
         self.ylim = self.ax.get_ylim()
-        self.ax.callbacks.connect('xlim_changed', self.lim_change)
-        self.ax.callbacks.connect('ylim_changed', self.lim_change)
-
-        super().__init__(self.fig)
 
         self.fig_factor = None
         self.path = None
-        self.point_sizes = None
+        self.psizes = None
 
-    def clearAxes(self):
-        self.ax.cla()
-
-    def lim_change(self, ax):
-        lx = ax.get_xlim()
-        ly = ax.get_ylim()
-
-        factor = min(
+    def getZoomFactor(self, lx: tuple[float, float], ly: tuple[float, float]) -> float:
+        return min(
             (self.xlim[1] - self.xlim[0]) / (lx[1] - lx[0]),
             (self.ylim[1] - self.ylim[0]) / (ly[1] - ly[0])
         )
 
+    def limChanged(self, ax: Axes):
+        lx = ax.get_xlim()
+        ly = ax.get_ylim()
+        zfactor = self.getZoomFactor(lx, ly)
+
         try:
-            self.path.set_sizes([s * factor * self.fig_factor for s in self.point_sizes])
+            self.path.set_sizes(
+                [s * zfactor * self.fig_factor for s in self.psizes]
+            )
         except KeyError:
             pass
 
-    def on_resize(self, event):
+    def onResize(self, event: Any):
         w = self.fig.get_figwidth()
         h = self.fig.get_figheight()
-        self.fig_factor = min(w / self.fig_width, h / self.fig_height)
-        self.lim_change(self.ax)
+        self.fig_factor = min(w / self.fig_w, h / self.fig_h)
 
-    def refreshDraw(self):
-        self.fig.canvas.draw()
-
-    def removeAxes(self):
-        axs = self.fig.axes
-        for ax in axs:
-            ax.remove()
-
-    def set_path(self, p):
-        self.path = p
-        self.point_sizes = p.get_sizes()
+    def setPath(self, path: PathCollection):
+        self.path = path
+        self.psizes = path.get_sizes()
 
 
 class Example(QMainWindow):
@@ -72,6 +67,15 @@ class Example(QMainWindow):
         super().__init__()
 
         chart = MyChart()
+        chart.ax.grid()
+        self.setCentralWidget(chart)
+
+        navbar = NavigationToolbar(chart, self)
+        self.addToolBar(Qt.ToolBarArea.BottomToolBarArea, navbar)
+
+        self.draw_chart(chart)
+
+    def draw_chart(self, chart: MyChart):
         list_x = list()
         list_y = list()
         for i in range(100):
@@ -79,13 +83,8 @@ class Example(QMainWindow):
             y = sin(x)
             list_x.append(x)
             list_y.append(y)
-        p = chart.ax.scatter(list_x, list_y, s=20, c='blue')
-        chart.ax.grid()
-        chart.set_path(p)
-        self.setCentralWidget(chart)
-
-        navbar = NavigationToolbar(chart, self)
-        self.addToolBar(Qt.ToolBarArea.BottomToolBarArea, navbar)
+        path = chart.ax.scatter(list_x, list_y, s=20, c='blue')
+        chart.setPath(path)
 
 
 def main():
