1ea893a0f8e9ad6f704e2a51a4c3eb580e3e3798
Fuhito Suguri <bitwalk@gmail.com>
2024-08-16 16:42:27 +0900

    update

--- qt_radiobutton_fruit_2.py
+++ qt_radiobutton_fruit_2.py
@@ -11,6 +11,16 @@ from PySide6.QtWidgets import (
 )
 
 
+class ToggleButtonImage(QPushButton):
+    def __init__(self, imgname: str, tooltip_str: str):
+        super().__init__()
+        self.setIcon(QIcon(imgname))
+        self.setIconSize(QSize(32, 32))
+        self.setToolTip(tooltip_str)
+        self.setCheckable(True)
+        self.setAutoExclusive(True)
+
+
 class Example(QWidget):
     def __init__(self):
         super().__init__()
@@ -19,30 +29,14 @@ class Example(QWidget):
         hbox = QHBoxLayout()
         self.setLayout(hbox)
 
-        size = QSize(32, 32)
-
-        pb_apple = QPushButton()
-        pb_apple.setIcon(QIcon('fruit_apple.png'))
-        pb_apple.setIconSize(size)
-        pb_apple.setToolTip('林檎')
-        pb_apple.setCheckable(True)
-        pb_apple.setAutoExclusive(True)
+        pb_apple = ToggleButtonImage('fruit_apple.png', '林檎')
         hbox.addWidget(pb_apple)
         pb_apple.toggle()
 
-        pb_grape = QPushButton()
-        pb_grape.setIcon(QIcon('fruit_grape.png'))
-        pb_grape.setIconSize(size)
-        pb_grape.setToolTip('葡萄')
-        pb_grape.setCheckable(True)
-        pb_grape.setAutoExclusive(True)
+        pb_grape = ToggleButtonImage('fruit_grape.png', '葡萄')
         hbox.addWidget(pb_grape)
 
-        pb_orange = QPushButton()
-        pb_orange.setIcon(QIcon('fruit_orange.png'))
-        pb_orange.setIconSize(size)
-        pb_orange.setToolTip('蜜柑')
-        pb_orange.setCheckable(True)
+        pb_orange = ToggleButtonImage('fruit_orange.png', '蜜柑')
         pb_orange.setAutoExclusive(True)
         hbox.addWidget(pb_orange)
 
