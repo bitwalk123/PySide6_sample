6633651e9da2d0af0733e4fc3176236cb0f0ebb0
Fuhito Suguri <bitwalk@gmail.com>
2024-08-17 13:44:54 +0900

    update

--- mpl_finance_3.py
+++ mpl_finance_3.py
@@ -5,6 +5,7 @@ import mplfinance as mpf
 import pandas as pd
 import sys
 
+from PySide6.QtCore import Qt, Signal
 from matplotlib.backends.backend_qtagg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar,
@@ -14,19 +15,17 @@ from PySide6 import QtCore
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
+    QToolBar, QCheckBox,
 )
 
 
 class MyChart(FigureCanvas):
     def __init__(self):
         self.fig = Figure()
+        self.ax = dict()
         super().__init__(self.fig)
-        self.ax = ax = dict()
 
-        n = 2
-        self.init_axes(ax, n)
-
-    def init_axes(self, ax, n: int):
+    def initAxes(self, ax, n: int):
         grid = plt.GridSpec(n + 1, 1, wspace=0.0, hspace=0.0)
         # Main
         ax[0] = self.fig.add_subplot(grid[0:2, 0])
@@ -40,39 +39,90 @@ class MyChart(FigureCanvas):
             if i < n - 1:
                 ax[i].tick_params(labelbottom=False)
 
+    def initChart(self, n: int):
+        self.removeAxes()
+        self.initAxes(self.ax, n)
+
+    def clearAxes(self):
+        axs = self.fig.axes
+        for ax in axs:
+            ax.cla()
+
+    def refreshDraw(self):
+        self.fig.canvas.draw()
+
+    def removeAxes(self):
+        axs = self.fig.axes
+        for ax in axs:
+            ax.remove()
+        self.ax = dict()
+
+
+class ChartNavigation(NavigationToolbar):
+    def __init__(self, chart: FigureCanvas):
+        super().__init__(chart)
+
+
+class MyToolBar(QToolBar):
+    volumeCheckChanged = Signal()
+
+    def __init__(self):
+        super().__init__()
+        self.check_volume = check_volume = QCheckBox('Volume')
+        check_volume.setChecked(True)
+        check_volume.checkStateChanged.connect(self.volume_check_changed)
+        self.addWidget(check_volume)
+
+    def volume_check_changed(self):
+        self.volumeCheckChanged.emit()
+
+    def isVolumeChecked(self) -> bool:
+        return self.check_volume.isChecked()
+
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        # sample dataset
+        filename = 'candlestick_sample_data.csv'
+        self.df = pd.read_csv(filename, index_col=0, parse_dates=True)
+
         self.resize(800, 600)
         self.setWindowTitle('Multiple charts')
+
+        self.toolbar = toolbar = MyToolBar()
+        toolbar.volumeCheckChanged.connect(self.draw)
+        self.addToolBar(QtCore.Qt.ToolBarArea.TopToolBarArea, toolbar)
 
         self.chart = chart = MyChart()
         self.setCentralWidget(chart)
 
-        self.addToolBar(
-            QtCore.Qt.ToolBarArea.BottomToolBarArea,
-            NavigationToolbar(chart, self)
-        )
+        self.navigation = navigation = ChartNavigation(chart)
+        self.addToolBar(QtCore.Qt.ToolBarArea.BottomToolBarArea, navigation)
 
         self.draw()
 
     def draw(self):
-        # sample dataset
-        filename = 'candlestick_sample_data.csv'
-        df = pd.read_csv(filename, index_col=0, parse_dates=True)
+        if self.toolbar.isVolumeChecked():
+            n = 2
+        else:
+            n = 1
 
+        self.chart.initChart(n)
         param = dict(
-            data=df,
+            data=self.df,
             style='yahoo',
             type='candle',
             datetime_format='%m/%d',
             xrotation=0,
             ax=self.chart.ax[0],
         )
-        param['volume'] = self.chart.ax[1]
+
+        if self.toolbar.isVolumeChecked():
+            param['volume'] = self.chart.ax[1]
 
         mpf.plot(**param)
+        self.chart.refreshDraw()
 
 
 def main():
